301d2bbf9b51791afaed4b89983024434ffc8ea0
Jef De Smedt <jef.desmedt@betavzw.org>
2018-09-11 10:05:58 +0200

    userlist added

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
 app.get('/', (req, res) => res.send('This is an API server.'));
 app.use('/api/courses',require('./training'));
+app.use('/api/users', require('./user'));
 app.listen(3000, () => console.log('Server has started'));
---
+++ user.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const users = [
+    {id:1, name:Joske, password:joske},
+    {id:2, name:Karen, password:karen},
+    {id:3, name:Kristel, password: kristel},
+    {id:4, name:Kathleen, password: kathleen}
+]
+router.get('/', (req, res) => {
+    console.log('Somebody wants to get the users.')
+    const overview = users.map(user => {
+        var obj = {};
+        obj['id'] = user.id;
+        obj['name'] = user.name;
+        return obj;
+    });
+    return res.status(200).json(overview);
+});
+
+module.exports = router;
